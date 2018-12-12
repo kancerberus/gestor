4346d3d73d7dd7b4a25a04bbb4c4858e4f6e2d38
kancerberus <cavl_21@hotmail.com>
2018-12-12 11:12:54 -0500

    Cambios por Carlos Villa, UtilidadesDAO

--- controller/src/main/java/com/gestor/controller/GestorPropiedades.java
+++ controller/src/main/java/com/gestor/controller/GestorPropiedades.java
@@ -15,11 +15,11 @@ public class GestorPropiedades {
     public Properties cargarPropiedades() throws Exception {
         Properties p = new Properties();
         try {
-//            p.setProperty("urlbd", "jdbc:postgresql://10.1.1.141:5432/gestor");
+//    p.setProperty("urlbd", "jdbc:postgresql://localhost:5432/gestor");
             p.setProperty("urlbd", "jdbc:postgresql://192.168.1.9:5432/gestor");
             p.setProperty("controlador", "org.postgresql.Driver");
             p.setProperty("usuario", "postgres");
-//            p.setProperty("clave", "postgres");
+//    p.setProperty("clave", "postgres");
             p.setProperty("rutaAdjunto", "c:\\gestor\\archivos");
             p.setProperty("rutaTemporal", "c:\\gestor\\tmp");
             p.setProperty("rutaEliminado", "c:\\gestor\\eliminados");
