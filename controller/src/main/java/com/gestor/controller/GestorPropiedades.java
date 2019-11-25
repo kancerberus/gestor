f1b5cb4475c296b2643dd5e4b7c4161416fbc947
kancerberus <cavl_21@hotmail.com>
2019-11-25 14:29:38 -0500

    Merge origin/master
    Conflicts:
    	controller/src/main/java/com/gestor/controller/GestorPropiedades.java
    	gestor-web/src/main/webapp/seguimiento/plan-maestro.xhtml

--- controller/src/main/java/com/gestor/controller/GestorPropiedades.java
+++ controller/src/main/java/com/gestor/controller/GestorPropiedades.java
@@ -5,7 +5,6 @@
 package com.gestor.controller;
 
 import java.util.Properties;
-
 /**
  *
  * @author juliano
@@ -16,15 +15,15 @@ public class GestorPropiedades {
         Properties p = new Properties();
         try {
 //    p.setProperty("urlbd", "jdbc:postgresql://localhost:5432/gestor");
-            p.setProperty("urlbd", "jdbc:postgresql://192.168.1.9:5432/gestor");
+            p.setProperty("urlbd", "jdbc:postgresql://192.168.1.29:5432/gestor");
             p.setProperty("controlador", "org.postgresql.Driver");
             p.setProperty("usuario", "postgres");
 //    p.setProperty("clave", "postgres");
             p.setProperty("rutaAdjunto", "c:\\gestor\\archivos");
             p.setProperty("rutaTemporal", "c:\\gestor\\tmp");
             p.setProperty("rutaEliminado", "c:\\gestor\\eliminados");            
-            //p.setProperty("guardarLogos", "D:\\gestor\\gestor-web\\target\\gestor-web-1.0\\resources\\imagenes\\establecimientos");
-            p.setProperty("guardarLogos", "c:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\webapps\\gestor-web\\resources\\imagenes\\establecimientos\\");  
+    //p.setProperty("guardarLogos", "D:\\gestor\\gestor-web\\target\\gestor-web-1.0\\resources\\imagenes\\establecimientos");
+            p.setProperty("guardarLogos", "c:\\tomcat9\\webapps\\gestor-web\\resources\\imagenes\\establecimientos\\");  
             p.setProperty("clave", "1234");
 
         } catch (Exception e) {
